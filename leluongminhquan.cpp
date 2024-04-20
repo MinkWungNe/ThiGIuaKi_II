3e1d5efcb82c28fe0c2245f1c15c39c52051cc82
MinkWungNe <leluongminhquan@gmail.com>
2024-04-20 14:00:41 +0700

    Finished and fixed launch.json

--- leluongminhquan.cpp
+++ leluongminhquan.cpp
@@ -4,30 +4,27 @@
 using namespace std;
 const int soluong = 5;
 
-//part 1
+//part 1 ---------------------------------------------------------------------------------------
 string danhsachMSSV[soluong] = {"","2305CT2353","2302CT3456","2305CT3999","2304CT0002"};
 
-//part 2
+//part 2----------------------------------------------------------------------------------------
 void NhapMSSV()
 {
     cout << "Nhap ma so sinh vien: ";
     getline(cin,danhsachMSSV[0]);
-    for (int i = 0; i < 5; i++)
-    {
-        cout << danhsachMSSV[i] << endl;
-    }
 }
 
-//part 3
+//part 3 ----------------------------------------------------------------------------------------
 void Indanhsach()
 {
-    for ( int i = 0; i < 5; i++)
+        cout << "Danh sach sinh vien: " << endl;
+    for ( int i = 0; i < soluong; i++)
     {
-        cout << i << ". " << danhsachMSSV[i] << endl;
+        cout << i+1 << ". " << danhsachMSSV[i] << endl;
     }
 }
 
-//part 4
+//part 4 -----------------------------------------------------------------------------------------
 void SapxepDS(string mang[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -38,10 +35,35 @@ void SapxepDS(string mang[], int n) {
         }
     }
 }
+void IndanhsachMoi()
+{
+    cout << "Danh sach sau sap xep: " << endl;
+    for ( int i = 0; i < 5; i++)
+    { 
+        cout << i+1 << ". " << danhsachMSSV[i] << endl;
+    }
+}
+
+//part 5 ------------------------------------------------------------------------------------------
+void Timdiachi()
+{
+    string maxMSSV = "2305CT3999";
+    string* ptrDanhsachMSSV = danhsachMSSV;
+    for (int i = 1; i < soluong; ++i) {
+        if (danhsachMSSV[i] == maxMSSV) {
+            cout << "Dia chi cua phan tu chua MSSV lon nhat la: " << (ptrDanhsachMSSV + i) << endl;
+            cout << "Gia tri cua phan tu do la: " << danhsachMSSV[i] << endl;
+            break;
+        }
+    }
+}
+    
 int main()
 {
     NhapMSSV();
     Indanhsach();
+    cout << endl;
     SapxepDS(danhsachMSSV, soluong);
-    Indanhsach();
+    IndanhsachMoi();
+    Timdiachi();
 }
